95cd3376f520f4ccb2648353ec1a5b044cd4f110
Jesse persinger <70872264+Mau5trap7791@users.noreply.github.com>
2020-11-03 22:06:16 -0500

    Original design and psedo

---
+++ script.js
@@ -0,0 +1,54 @@
+// Makin it purty
+
+// Variables
+
+var title = 
+
+var question = 
+
+var timeLeft =
+
+var score =
+
+var choice1 =
+
+var choice2 =
+
+var choice3 =
+
+var choice4 = 
+
+var submit =
+
+//
+
+
+// Arrays, question as name answers as values.
+
+
+
+// Functions
+
+
+//on init alert start quiz
+
+
+//change question to first in array
+
+
+//change answers to first array
+
+
+//start timer
+
+
+//onclick submit
+
+
+//check answer against stored value
+
+
+//add to score else alert incorect
+
+
+//change questions and answers to secound array.
